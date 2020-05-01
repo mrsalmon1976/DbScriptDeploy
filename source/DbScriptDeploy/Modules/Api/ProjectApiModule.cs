c62a29b6be7874dc9cf3915c12fbd0350402895f
Matt Salmon <matt@iua.co.za>
2020-05-01 19:44:36 +0200

    Added unit test for project api module

--- source/DbScriptDeploy/Modules/Api/ProjectApiModule.cs
+++ source/DbScriptDeploy/Modules/Api/ProjectApiModule.cs
@@ -12,15 +12,31 @@ namespace DbScriptDeploy.Modules.Api
     {
         public const string Route_Post = "/api/projects";
 
+        public const string Route_Get_Api_Projects_User = "/api/projects/user";
+
+        private readonly IProjectCreateCommand _projectCreateCommand;
+
         public ProjectApiModule(IProjectCreateCommand projectCreateCommand)
         {
+            _projectCreateCommand = projectCreateCommand;
+
             Post(Route_Post, x =>
             {
-                var projectName = Request.Form.ProjectName;
-                ProjectModel result = projectCreateCommand.Execute(projectName);
-                return this.Response.AsJson(result);
+                return AddProject();
             });
+            //Get(Route_Get_Api_Projects_User, x =>
+            //{
+            //    ProjectModel result = projectCreateCommand.Execute(projectName);
+            //    return this.Response.AsJson(result);
+            //});
 
+        }
+
+        public dynamic AddProject()
+        {
+            var projectName = Request.Form.ProjectName;
+            ProjectModel result = _projectCreateCommand.Execute(projectName);
+            return this.Response.AsJson(result);
         }
     }
 }
---
+++ source/Test.DbScriptDeploy/Modules/Api/ProjectApiModuleTest.cs
@@ -0,0 +1,88 @@
+﻿using DbScriptDeploy.BLL.Commands;
+using DbScriptDeploy.BLL.Models;
+using DbScriptDeploy.BLL.Repositories;
+using DbScriptDeploy.BLL.Security;
+using DbScriptDeploy.Modules.Api;
+using DbScriptDeploy.Security;
+using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Bootstrapper;
+using Nancy.Responses.Negotiation;
+using Nancy.Testing;
+//using Nancy.Testing;
+using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.DbScriptDeploy.Modules.Api
+{
+    [TestFixture]
+    public class ProjectApiModuleTest
+    {
+        private IProjectCreateCommand _projectCreateCommand;
+
+        [SetUp]
+        public void ProjectApiModuleTest_SetUp()
+        {
+            _projectCreateCommand = Substitute.For<IProjectCreateCommand>();
+        }
+
+        #region AddProject Tests
+
+        [Test]
+        public void AddProject_ValidData_ReturnsProjectData()
+        {
+            // setup
+            var currentUser = new ClaimsPrincipal(new GenericPrincipal(new GenericIdentity("Joe Soap"), new string[] { }));
+            var browser = CreateBrowser(currentUser);
+            var user = DataHelper.CreateUserModel();
+
+
+            ProjectModel project = DataHelper.CreateProjectModel();
+            _projectCreateCommand.Execute(project.Name).Returns(project);
+
+            // execute
+            var response = browser.Post(ProjectApiModule.Route_Post, (with) =>
+            {
+                with.HttpRequest();
+                with.FormValue("projectName", project.Name);
+                with.FormsAuth(user.Id, new FormsAuthenticationConfiguration());
+            }).Result;
+
+            // assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            _projectCreateCommand.Received(1).Execute(project.Name);
+
+            ProjectModel result = JsonConvert.DeserializeObject<ProjectModel>(response.Body.AsString());
+            Assert.AreEqual(project.Id, result.Id);
+        }
+
+ 
+        #endregion
+
+        #region Private Methods
+
+        private Browser CreateBrowser(ClaimsPrincipal currentUser)
+        {
+
+            var browser = new Browser((bootstrapper) =>
+                            bootstrapper.Module(new ProjectApiModule(_projectCreateCommand))
+                                .RootPathProvider(new TestRootPathProvider())
+                                .RequestStartup((container, pipelines, context) => {
+                                    context.CurrentUser = currentUser;
+                                })
+                            );
+            return browser;
+        }
+
+        #endregion
+    }
+}
